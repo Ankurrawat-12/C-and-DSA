92ded21bc701fcc88072df6781c74d2e16c05e23
Ankur Rawat <ankurrawat620@gmail.com>
2023-03-09 01:50:52 +0530

    Add 2549

---
+++ LeetCode/2549.count-distinct-numbers-on-board.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2549 lang=cpp
+ *
+ * [2549] Count Distinct Numbers on Board
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int distinctIntegers(int n) {
+        
+    }
+};
+// @lc code=end
+
